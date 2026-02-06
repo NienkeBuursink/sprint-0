8b27025b0493230c9d7b8e63242535ac9b48abad
Nienke <152287217+NienkeBuursink@users.noreply.github.com>
2026-02-06 12:59:13 +0100

    voor de expo
    even voor de zekerheid

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,11 @@
-const snaren = document.querySelectorAll('.snaar-audio');
+const snaren = document.querySelectorAll('.snaar');
 
 snaren.forEach((snaar) => {
-
-})
+    ['mouseover', 'click'].forEach(event => // array met events voor hover op desktop en click op desktop en mobile
+        snaar.addEventListener(event, function(e) {
+            let snaarAudio = snaar.nextElementSibling; // sibling die direct onder de div staat, dus audio
+            // console.log(snaarAudio);
+            snaarAudio.play();
+        })
+    );
+});
